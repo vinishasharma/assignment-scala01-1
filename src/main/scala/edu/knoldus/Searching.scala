3fdaa1b81186ace1d756f468a873037d54aaefca
vinishasharma <vinisha.sharma@knoldus.in>
2018-01-08 17:50:13 +0530

    assignment-scala01-1_vinisha

---
+++ src/main/scala/edu/knoldus/Searching.scala
@@ -0,0 +1,46 @@
+package edu.knoldus
+
+class Searching {
+
+  def binarySearch(array: Array[Int], elem: Int): Boolean = {
+
+    val position:Int = binarySear(array,0,8,elem)
+    if(position == -1) {
+      println("number present at position " + position)
+      false
+    }
+    else {
+      true
+    }
+  }
+
+  def linearSearch(array: Array[Int], elem: Int): Boolean = {
+    for (i <- 0 to (array.length - 1)) {
+      if (array(i) == elem) {
+        println("number found at position " + i)
+        return true
+      }
+    }
+
+    false
+  }
+
+  def binarySear(sortedArray: Array[Int], start: Int, end: Int, key: Int): Int =  {
+
+    if (start < end) {
+      val mid: Int = start + (end - start) / 2
+      if (key < sortedArray(mid)) {
+        return binarySear(sortedArray, start, mid, key)
+
+      }
+      else if (key > sortedArray(mid)) {
+        return binarySear(sortedArray, mid + 1, end, key);
+      }
+      else {
+        return mid;
+      }
+    }
+    return -1
+  }
+
+}
---
+++ src/main/scala/edu/knoldus/Sorting.scala
@@ -0,0 +1,54 @@
+package edu.knoldus
+
+class Sorting {
+
+  def insertionSort(array: Array[Int]): Array[Int] = {
+    for(i <- 0 until array.length){
+
+      val position = array(i)
+      var holdingPosition = i
+
+      while(holdingPosition > 0 && position < array(holdingPosition - 1)){
+        array(holdingPosition) = array(holdingPosition - 1)
+        holdingPosition -= 1
+      }
+
+      array(holdingPosition) = position
+    }
+
+    array
+  }
+
+  def selectionSort(array: Array[Int]): Array[Int] = {
+    for(i <- 0 until array.length){
+      var position = i
+      for(j <- i + 1 until array.length)
+        if(array(j) < array(position)) {
+          position = j
+        }
+
+      if(position != i){
+        val temp = array(i)
+        array(i) = array(position)
+        array(position) = array(i)
+      }
+    }
+
+    array
+    
+  }
+
+  def bubbleSort(array: Array[Int]): Array[Int] = {
+    for (i <- (0 until array.length)) {
+      for (j <- (0 until array.length)) {
+        if (array(i) < array(j)) {
+          val swap = array(i)
+          array(i) = array(j)
+          array(j) = swap
+        }
+      }
+    }
+    array
+  }
+
+}
